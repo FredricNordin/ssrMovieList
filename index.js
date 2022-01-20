e2f79306f361b1ee0dadcaf38078ccda51812fd3
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-01-20 09:11:02 +0100

    create basic server render

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import fs from "fs/promises";
+const server = express();
+
+
+server.get('/', async (request, response) => {
+  const fileRender = await fs.readFile('./src/index.html');
+  response.type('html');
+  response.send(fileRender);
+});
+
+app.listen(5080);
+console.log('SERVER IS UP AND RUNNING ON PORT 5080')
